ee035cc3d84210ce9fa580879a802d976f96bc49
Your Name <you@example.com>
2024-03-03 21:10:24 +0630

    started

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -18,6 +18,7 @@ const Main = () => {
         </button>
         <button
           onMouseOver={handlePosition}
+          onMouseLeave={handlePosition}
           class={`absolute bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded-full`}
           style={{ bottom: point + "%", left: point + "%" }}
         >
